7bfac4c6f5f413ced00393f8fcb2f3c3e89ef431
Alexey Grichenko <a8grich@gmaiil.com>
2022-06-07 14:05:10 +0300

    master

---
+++ __init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'ew342f2f23mb4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    
+    # blueprint for auth routes
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non auth parts 
+    from .auth import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
