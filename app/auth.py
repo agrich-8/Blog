0a7617ba6f994c8cfbe85cad1f4db490bfd04f77
Alexey Grichenko <a8grich@gmaiil.com>
2022-09-10 17:59:58 +0300

    modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-310.pyc 	modified:   app/__pycache__/auth.cpython-310.pyc 	modified:   app/__pycache__/main.cpython-310.pyc 	modified:   app/__pycache__/models.cpython-310.pyc 	modified:   app/auth.py 	modified:   app/db.mwb 	modified:   app/db.mwb.bak 	modified:   app/main.py 	modified:   app/models.py 	modified:   app/templates/base.html 	modified:   app/templates/login.html 	modified:   app/templates/profile.html

--- app/auth.py
+++ app/auth.py
@@ -1,27 +1,73 @@
 from flask import Blueprint, render_template, redirect, url_for,flash, request
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, SignUpForm
+from flask_login import login_user
+from .models import User, SignUpForm, LoginForm
 from . import db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'PAST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     email = None
     password = None
-    form = SignUpForm()
+    remember = None
+    form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
             print("xyu")
             email = form.email.data
             password = form.password.data
+            remember = True if form.checkbox.data else False
             form.email.data = ''
             form.password.data = ''
+            form.checkbox.data = False
             user = User.query.filter_by(email=email).first()
             if user:
-                return redirect(url_for('auth.signup'))
-                
+                if check_password_hash(user.password, password):
+                    login_user(user, remember=remember)
+                    return redirect(url_for('main.profile'))
+                else:
+                    flash('Please enter the password you provided on sign up')
+                    return redirect(url_for('auth.login'))
+            else:
+                flash('Please enter the email you provided on sign up')
+                return redirect(url_for('auth.login'))
+        elif not email:
+            flash('Email is not specified, please try again.')
             return redirect(url_for('auth.login'))
+        elif not password:
+            flash('Password is not specified, please try again.')
+            return redirect(url_for('auth.login'))
+    return render_template('login.html', form=form, email=email,
+                            password=password, checkbox=remember
+                            )
+
+
+@auth.route('/login_check', methods=['GET', 'POST'])
+def login_check():
+    email = None
+    password = None
+    remember = None
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print("xyu")
+            email = form.email.data
+            password = form.password.data
+            remember = True if form.checkbox.data else False
+            form.email.data = ''
+            form.password.data = ''
+            form.checkbox.data = False
+            user = User.query.filter_by(email=email).first()
+            if user:
+                if check_password_hash(user.password, password):
+                    return redirect(url_for('main.profile'))
+                else:
+                    flash('Please enter the password you provided on sign up')
+                    return redirect(url_for('auth.login_check'))
+            else:
+                flash('Please enter the email you provided on sign up')
+                return redirect(url_for('auth.login_check'))
         elif not email:
             flash('Email is not specified, please try again.')
             return redirect(url_for('auth.signup'))
@@ -29,8 +75,9 @@ def login():
             flash('Password is not specified, please try again.')
             return redirect(url_for('auth.signup'))
     return render_template('login.html', form=form, email=email,
-                            password=password
+                            password=password, checkbox=remember
                             )
+
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -51,13 +98,14 @@ def signup():
             form.name.data = ''
             form.email.data = ''
             form.password.data = ''
-            form.checkbox.data = ''
+            form.checkbox.data = False
             form.password_repeat.data = ''
             user = User.query.filter_by(email=email).first()
             if password != password_repeat:
                 flash('You entered two different passwords. Please try again.')
                 return redirect(url_for('auth.signup'))
             if user:
+                flash('Email address already exists')
                 return redirect(url_for('auth.signup'))
             hash = generate_password_hash(
                 password,
@@ -67,6 +115,7 @@ def signup():
             new_user = User(email=email, name=name, password=hash)
             db.session.add(new_user)
             db.session.commit()
+
             return redirect(url_for('auth.login'))
         elif not name:
             flash('Name is not specified, please try again.')
--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from . import db
+from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
@@ -13,9 +14,17 @@ class User(db.Model):
     path = db.Column(db.String(44))
 
 class SignUpForm(FlaskForm):
+
     name =  StringField('Name', validators=[DataRequired()])
     email =  StringField('Email', validators=[DataRequired()])
     password =  StringField('Password', validators=[DataRequired()])
     password_repeat = StringField('PasswordRepeat', validators=[DataRequired()])
     checkbox = BooleanField('CheckBox', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+
+    email =  StringField('Email', validators=[DataRequired()])
+    password =  StringField('Password', validators=[DataRequired()])
+    checkbox = BooleanField('CheckBox', validators=[DataRequired()])
+    submit = SubmitField('Login')
