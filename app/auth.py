0e592c37cd1453351cbfdd9d21917ef5330c0a8b
Alexey Grichenko <a8grich@gmaiil.com>
2022-09-26 22:18:03 +0300

    working email

--- app/auth.py
+++ app/auth.py
@@ -1,87 +1,19 @@
 from datetime import datetime
 from flask import Blueprint, render_template, redirect, url_for,flash, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+# from flask_jwt_extended import current_user as current_user_jwt
+
 from .models import User
 from .forms import SignUpForm, LoginForm
+from .email import send_email
 from . import db
 
 auth = Blueprint('auth', __name__)
-
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    email = None
-    password = None
-    remember = None
-    form = LoginForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            print("xyu")
-            email = form.email.data
-            password = form.password.data
-            remember = True if form.checkbox.data else False
-            form.email.data = ''
-            form.password.data = ''
-            form.checkbox.data = False
-            user = User.query.filter_by(email=email).first()
-            if user:
-                if user.verify_password(password):
-                    login_user(user, remember=remember)
-                    # send_email(user.email, 'Confirm Your Account',
-                    #             'auth/email/confirm', user=user,
-                    #             token=token)
-                    flash('A confirmation email has been sent to you by email.')
-                    return redirect(url_for('main.profile'))
-                else:
-                    flash('Please enter the password you provided on sign up')
-                    return redirect(url_for('auth.login'))
-            else:
-                flash('Please enter the email you provided on sign up')
-                return redirect(url_for('auth.login'))
-        elif not email:
-            flash('Email is not specified, please try again.')
-            return redirect(url_for('auth.login'))
-        elif not password:
-            flash('Password is not specified, please try again.')
-            return redirect(url_for('auth.login'))
-    return render_template('login.html', form=form, email=email,
-                            password=password, checkbox=remember
-                            )
-
-
-# @auth.route('/login_check', methods=['GET', 'POST'])
-# def login_check():
-#     email = None
-#     password = None
-#     remember = None
-#     form = LoginForm()
-#     if request.method == 'POST':
-#         if form.validate_on_submit():
-#             print("xyu")
-#             email = form.email.data
-#             password = form.password.data
-#             remember = True if form.checkbox.data else False
-#             form.email.data = ''
-#             form.password.data = ''
-#             form.checkbox.data = False
-#             user = User.query.filter_by(email=email).first()
-#             if user:
-#                 if check_password_hash(user.password, password):
-#                     return redirect(url_for('main.profile'))
-#                 else:
-#                     flash('Please enter the password you provided on sign up')
-#                     return redirect(url_for('auth.login_check'))
-#             else:
-#                 flash('Please enter the email you provided on sign up')
-#                 return redirect(url_for('auth.login_check'))
-#         elif not email:
-#             flash('Email is not specified, please try again.')
-#             return redirect(url_for('auth.signup'))
-#         elif not password:
-#             flash('Password is not specified, please try again.')
-#             return redirect(url_for('auth.signup'))
-#     return render_template('login.html', form=form, email=email,
-#                             password=password, checkbox=remember
-#                             )
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -117,10 +49,13 @@ def signup():
             #     method='pbkdf2:sha256',
             #     salt_length=8
             # )
-            new_user = User(email=email, name=name, password=password, token='qwerty1233', created=str(datetime.now(tz=None))[:19])
+            token = User.generate_confirmation_token(current_user.name)
+            new_user = User(email=email, name=name, password=password, token='tk', created=str(datetime.now(tz=None))[:19])
             db.session.add(new_user)
             db.session.commit()
-
+            send_email(new_user.email, 'Confirm Your Account',
+                'auth/email/confirm', user=new_user.name,
+                token=token)
             return redirect(url_for('auth.login'))
         elif not name:
             flash('Name is not specified, please try again.')
@@ -138,10 +73,59 @@ def signup():
                             password=password, password_repeat=password_repeat, checkbox=checkbox
                             )
 
-@auth.route('/conrirm')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    email = None
+    password = None
+    remember = None
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print("xyu")
+            email = form.email.data
+            password = form.password.data
+            remember = True if form.checkbox.data else False
+            form.email.data = ''
+            form.password.data = ''
+            form.checkbox.data = False
+            user = User.query.filter_by(email=email).first()
+            if user:
+                if user.verify_password(password):
+                    login_user(user, remember=remember)
+                    # send_email(user.email, 'Confirm Your Account',
+                    #             'auth/email/confirm', user=user,
+                    #             token=token)
+                    
+                    flash('A confirmation email has been sent to you by email.')
+                    return redirect(url_for('main.profile'))
+                else:
+                    flash('Please enter the password you provided on sign up')
+                    return redirect(url_for('auth.login'))
+            else:
+                flash('Please enter the email you provided on sign up')
+                return redirect(url_for('auth.login'))
+        elif not email:
+            flash('Email is not specified, please try again.')
+            return redirect(url_for('auth.login'))
+        elif not password:
+            flash('Password is not specified, please try again.')
+            return redirect(url_for('auth.login'))
+    return render_template('login.html', form=form, email=email,
+                            password=password, checkbox=remember
+                            )
+
+
+@auth.route('/conrirm/<token>')
 @login_required
-def confirm():
-    
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
 
 @auth.route('/logout')
