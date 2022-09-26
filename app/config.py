ba55b34f7b8eff9e09c8fa3453b0da89370b70c8
Alexey Grichenko <a8grich@gmaiil.com>
2022-09-26 22:18:13 +0300

    modified:   app/__pycache__/auth.cpython-310.pyc 	modified:   app/__pycache__/config.cpython-310.pyc 	modified:   app/__pycache__/email.cpython-310.pyc 	modified:   app/__pycache__/main.cpython-310.pyc 	modified:   app/config.py 	modified:   app/email.py 	modified:   app/main.py

--- app/config.py
+++ app/config.py
@@ -18,11 +18,11 @@ class DevelopmentConﬁg(Conﬁg):
     MAIL_PORT = 587
     MAIL_DEBUG = True
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <xxxxxxx@yandex.ru>'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')             
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')             
+    MAIL_DEFAULT_SENDER  = os.environ.get('MAIL_USERNAME')      
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASK BLOG]'
+    FLASKY_MAIL_SENDER = f"Flasky Admin <{os.environ.get('MAIL_USERNAME')}>"
     FLASKY_ADMIN = os.environ.get('MAIL_USERNAME')
 
 class TestingConﬁg(Conﬁg): 
--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from flask_jwt_extended import jwt_required
 from .models import User
-from . import db
 from .email import send_email
+from . import db
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -17,5 +18,6 @@ def profile():
     send_email('alex.go.888.1@gmail.com', 'Confirm Your Account',
                 'auth/email/confirm', user=current_user.name,
                 token=token)
-    return render_template('profile.html', name=token) #name=current_user.name)
+    
+    return render_template('profile.html', name=current_user.name, retok=token) #name=current_user.name)
 
